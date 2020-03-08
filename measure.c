997c01600d229634fe8f29b90ace0cc52c56ddac
WKD622 <kuba.ziarko@gmail.com>
2020-03-08 13:27:55 +0100

    first

--- measure.c
+++ measure.c
@@ -14,7 +14,7 @@ void initialize_table(int *t,int number_of_elements){
 void print_table(int *t, int number_of_elements){
   int i;
   for (i = 0; i < number_of_elements; i++){
-      printf("%i ", t[i])
+      printf("%i ", t[i]);
   }
 }
 
