1c3cd16fc8ec4d51b0fc5f882eec05a3e6cf1112
WKD622 <kuba.ziarko@gmail.com>
2020-03-09 16:55:04 +0100

    commit

--- lab.c
+++ lab.c
@@ -4,28 +4,48 @@
 #include <time.h>
 #include <sys/time.h>
 
-if (world_rank == 0) {
-  rand_nums = create_rand_nums(elements_per_proc * world_size);
-}
+int main(int argc, char **argv)
+{
+    MPI_Init(NULL, NULL);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-float *sub_rand_nums = malloc(sizeof(float) * elements_per_proc);
+    // We are assuming at least 2 processes for this task
+    if (world_size < 2)
+    {
+        fprintf(stderr, "World size must be greater than 1 for %s\n", argv[0]);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    int number;
 
-// Scatter the random numbers to all processes
-MPI_Scatter(rand_nums, elements_per_proc, MPI_FLOAT, sub_rand_nums,
-            elements_per_proc, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    if (world_rank == 0)
+    {
+        rand_nums = create_rand_nums(elements_per_proc * world_size);
+    }
 
-// Compute the average of your subset
-float sub_avg = compute_avg(sub_rand_nums, elements_per_proc);
+    float *sub_rand_nums = malloc(sizeof(float) * elements_per_proc);
 
-// Gather all partial averages down to the root process
-float *sub_avgs = NULL;
-if (world_rank == 0) {
-  sub_avgs = malloc(sizeof(float) * world_size);
-}
-MPI_Gather(&sub_avg, 1, MPI_FLOAT, sub_avgs, 1, MPI_FLOAT, 0,
-           MPI_COMM_WORLD);
+    // Scatter the random numbers to all processes
+    MPI_Scatter(rand_nums, elements_per_proc, MPI_FLOAT, sub_rand_nums,
+                elements_per_proc, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-// Compute the total average of all numbers.
-if (world_rank == 0) {
-  float avg = compute_avg(sub_avgs, world_size);
+    // Compute the average of your subset
+    float sub_avg = compute_avg(sub_rand_nums, elements_per_proc);
+
+    // Gather all partial averages down to the root process
+    float *sub_avgs = NULL;
+    if (world_rank == 0)
+    {
+        sub_avgs = malloc(sizeof(float) * world_size);
+    }
+    MPI_Gather(&sub_avg, 1, MPI_FLOAT, sub_avgs, 1, MPI_FLOAT, 0,
+               MPI_COMM_WORLD);
+
+    // Compute the total average of all numbers.
+    if (worūld_rank == 0)
+    {
+        float avg = compute_avg(sub_avgs, world_size);
+    }
 }
