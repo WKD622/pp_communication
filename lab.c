85a5a6d7f93c78fbdf82a1847620ed7b4493b3f8
WKD622 <kuba.ziarko@gmail.com>
2020-03-09 17:29:08 +0100

    commit

--- lab.c
+++ lab.c
@@ -66,8 +66,8 @@ int main(int argc, char **argv)
     int num_elements_per_proc = atoi(argv[1]);
 
     srand(time(NULL));
-    int number_of_nodes = 3 MPI_Init(NULL, NULL);
-
+    int number_of_nodes = 3;
+    MPI_Init(NULL, NULL);
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     int world_size;
